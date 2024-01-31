7e2354bf8948f06772bf534774bbfd5c5936c6a0
Sean Guerrero <seanguerrero549@gmail.com>
2024-01-30 16:40:23 -0800

    finished day 13

---
+++ script.py
@@ -0,0 +1,54 @@
+############DEBUGGING#####################
+
+# # Describe Problem
+# def my_function():
+#   for i in range(1, 21):
+#     if i == 20:
+#       print("You got it")
+# my_function()
+# # range omits last number in range
+
+
+# # Reproduce the Bug
+# from random import randint
+# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(0, 5)
+# print(dice_imgs[dice_num])
+# # numbers to be randomly generated are for the index not the actual dice
+
+
+# # Play Computer
+# year = int(input("What's your year of birth?"))
+# if year > 1980 and year < 1994:
+#   print("You are a millenial.")
+# elif year >= 1994:
+#   print("You are a Gen Z.")
+# # there is no equal for 1994
+
+# # Fix the Errors
+# age = int(input("How old are you?"))
+# if age > 18:
+#     print(f"You can drive at age {age}.")
+# #indent error as well as need to have the input be an int. You also need an f string to include a variable value
+
+
+# #Print is Your Friend
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page = int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
+# # ==(equality) not good and needs to be =(assignment) instead
+
+
+#Use a Debugger
+def mutate(a_list):
+  b_list = []
+  for item in a_list:
+    new_item = item * 2
+    b_list.append(new_item)
+  print(b_list)
+
+mutate([1,2,3,5,8,13])
+# the append was not in the for loop
